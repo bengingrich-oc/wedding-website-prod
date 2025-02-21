5797d9676b914db7f6e71b2ffd9e81cb6df74831
oc-bengingrich <ben.gingrich@owenscorning.com>
2025-02-21 14:03:03 -0500

    Fixes to photo load delay

--- src/js/main.js
+++ src/js/main.js
@@ -57,15 +57,18 @@
 
 
     // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
+    var $portfolioContainer = $('.portfolio-container');
+    $portfolioContainer.imagesLoaded(function() {
+        var portfolioIsotope = $portfolioContainer.isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+        $('#portfolio-flters li').on('click', function () {
+            $("#portfolio-flters li").removeClass('active');
+            $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+            portfolioIsotope.isotope({ filter: $(this).data('filter') });
+        });
     });
     
     
